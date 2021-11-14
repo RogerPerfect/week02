903438b56b9b6ea882a1b73d1ad70f56e172746d
roger <729601116@qq.com>
2021-11-14 22:30:52 +0800

    提交初始demo

---
+++ src/test/java/com/example/java02/Java02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
